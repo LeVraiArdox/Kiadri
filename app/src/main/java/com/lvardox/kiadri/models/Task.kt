d815a0a2d568108b65faccecac43adf030997ab2
Ardox <ardox@axos-project.com>
2026-02-18 14:52:11 +0100

    Added completion date to Task

--- app/src/main/java/com/lvardox/kiadri/models/Task.kt
+++ app/src/main/java/com/lvardox/kiadri/models/Task.kt
@@ -4,5 +4,6 @@ data class Task(
     val id: String = java.util.UUID.randomUUID().toString(),
     val title: String,
     val isCompleted: Boolean = false,
-    val photoUri: String? = null
+    val photoUri: String? = null,
+    val completedAt: Long? = null
 )
